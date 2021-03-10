a7cb91a0e1a163fe33c06eb95752c891bcae259c
vijitha <kondapurvr@gmil.com>
2021-03-10 23:31:31 +0530

    added system

--- src/test/java/stepdefinitions/Amazonprice.java
+++ src/test/java/stepdefinitions/Amazonprice.java
@@ -3,10 +3,10 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 
 public class Amazonprice {
-	
+
 	@Given("I have a search field on Amazon Page")
 	public void i_have_a_search_field_on_amazon_page() {
-	    System.out.println("Step 1 : I am on Amazon Search Page webhook for Amazon price");
+		System.out.println("Step are first : I am on Amazon Search Page webhook for Amazon price");
 	}
 
 }
